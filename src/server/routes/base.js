a26efc7c5bf45054d415f27ba958dbcd67b4244d
h3nrey <pedro.novaes.education@gmail.com>
2022-04-19 13:40:18 -0300

    some updates on lockscreen form

--- src/server/routes/base.js
+++ src/server/routes/base.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const homeController = require("../controllers/home");
 const createUser = require("../controllers/createUserController");
+const res = require("express/lib/response");
 
 router.get("/", (req,res) => {
     res.render("index", {message : null});
@@ -9,5 +10,8 @@ router.get("/", (req,res) => {
 
 router.post("/home/:username", homeController.home);
 router.post("/", createUser.insert);
+router.get("/home", (req,res) => {
+    res.render("home");
+})
 
 module.exports = router;
